cff53ed4619bdb8fc5fd730b0618fee093be8a5b
Tai Studio <tai.studio@outlook.fr>
2022-06-17 01:03:49 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,10 @@ sudo apt install git
 sudo apt install feh
 
 feh --bg-scale $PWD/background.png
+
+
+
+sudo apt-get remove ubuntu-gnome-desktop
+sudo apt-get remove gnome-shell 
+
+sudo apt autoremove
