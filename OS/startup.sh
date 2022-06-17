f331758c3fdbf7de5db8179492284a1eb1459c22
Tai Studio <tai.studio@outlook.fr>
2022-06-17 13:32:28 +0200

    Update startup.sh

--- OS/startup.sh
+++ OS/startup.sh
@@ -1,13 +1,13 @@
 if [ ! -d 'DashiumOS/Dashium' ];
 then
-  mkdir DashiumOS/Dashium
-  mkdir DashiumOS/Dashium/apps
-  cd DashiumOS/Dashium
+  mkdir /DashiumOS/Dashium
+  mkdir /DashiumOS/Dashium/apps
+  cd /DashiumOS/Dashium
   git clone "https://github.com/Dashium/Dashium"
   cd Dashium
   npm setup
 else
   bash /home/dashium/DashiumInstaller/setup/install_DashiumOS.sh
-  cd DashiumOS/Dashium
+  cd /DashiumOS/Dashium
   npm start
 fi
