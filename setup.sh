1cb5ec587095a66bf2ea061c1adf0ce3357e2e94
Tai Studio <tai.studio@outlook.fr>
2022-06-20 15:25:53 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -84,7 +84,7 @@ fi
 
 if [ "$color_prompt" = yes ]; then
     # PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
-    PS1="\e[0;36m[DashiumOS]] [0;35m[\u]] [0;36m[@]]\h \W]\$ \e[0m"
+    PS1="\e[0;36m[DashiumOS]] \e[0;35m[\u]] \e[0m[@]] \e[0;32m[\W]] ]\$ \e[0m"
 else
     PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
 fi
